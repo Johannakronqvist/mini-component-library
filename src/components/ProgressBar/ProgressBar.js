b90667796d1d64092b128fbb54699a35e8e5fc43
Johannakronqvist <78489287+Johannakronqvist@users.noreply.github.com>
2023-12-06 11:54:35 +0100

    made dynamic progressbar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,46 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const WrapperBase = styled.div`
+  width: 100%;
+  border-radius: 4px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
+`;
+
+const Size = {
+  large: `--height: 24px; --padding: 4px;`,
+  medium: `--height: 16px; --padding: 0;`,
+  small: `--height: 8px; --padding: 0;`,
+};
+
+const Wrapper = styled(WrapperBase)`
+  ${({ size }) => Size[size]}
+  height: var(--height);
+  padding: var(--padding);
+`;
+
+const Progress = styled.div`
+  width: ${(props) => props.value}%;
+  height: 100%;
+  background-color: ${COLORS.primary};
+  border-radius: ${(props) =>
+    props.value === 100 ? '4px' : '4px 0px 0px 4px'};
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper
+      role='progressbar'
+      aria-valuenow={value}
+      aria-valuemax={100}
+      size={size}
+    >
+      <Progress value={value}>
+        <VisuallyHidden>{value}%</VisuallyHidden>
+      </Progress>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
