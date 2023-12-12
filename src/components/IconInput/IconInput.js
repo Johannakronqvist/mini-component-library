f05bbb2cde5bbf5563c05e3b35915ae65c79d1e4
Johannakronqvist <78489287+Johannakronqvist@users.noreply.github.com>
2023-12-12 10:59:42 +0100

    created IconInput component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,60 +6,74 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const Wrapper = styled.div`
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
+
+const Wrapper = styled.label`
+  display: block;
   position: relative;
-  color: ${COLORS.black};
-  width: ${(props) =>
-    props.size === 'large'
-      ? '300px'
-      : props.size === 'small'
-      ? '150px'
-      : 'inherit'};
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+  }
 `;
 
 const TextInput = styled.input`
   border: none;
-  border-bottom: solid 2px ${COLORS.black};
-  width: 100%;
-  padding: 4px;
-  padding-left: 28px;
-  font-size: ${14 / 16}rem;
-  color: ${COLORS.gray700};
+  border-bottom: solid var(--border-thickness) ${COLORS.black};
+  height: var(--height);
+  width: var(--width);
+  padding-left: var(--height);
+  color: inherit;
   font-weight: 700;
-
-  color: ${COLORS.gray500};
+  font-size: var(--fontSize);
+  outline-offset: 2px;
   &::placeholder {
     color: ${COLORS.gray500};
-  }
-  &:hover {
-    color: ${COLORS.black};
-  }
-  &:focus {
-    outline-offset: 2px;
-    border-radius: 2px;
+    font-weight: 400;
   }
 `;
 
 const IconWrapper = styled.div`
   position: absolute;
-  left: 4px;
-  top: 4px;
-  bottom: 4px;
-  pointer-events: none;
-  color: ${COLORS.gray700};
+  top: 0px;
+  bottom: 0px;
+  height: var(--size);
+  margin: auto 0;
 `;
 
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const styles = STYLES[size];
   return (
     <>
-      <Wrapper size={size} style={{ width: { width } }}>
-        <VisuallyHidden>
-          <label for='textInput'>{label}</label>
-        </VisuallyHidden>
-        <IconWrapper>
-          <Icon id={icon} size={16} />
+      <Wrapper>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
+          <Icon id={icon} size={styles.iconSize} />
         </IconWrapper>
-        <TextInput type='text' id='textInput' placeholder={placeholder} />
+        <TextInput
+          type='text'
+          id='textInput'
+          placeholder={placeholder}
+          style={{
+            '--width': width + 'px',
+            '--height': styles.height / 16 + 'rem',
+            '--border-thickness': styles.borderThickness + 'px',
+            '--fontSize': styles.fontSize / 16 + 'rem',
+          }}
+        />
       </Wrapper>
     </>
   );
